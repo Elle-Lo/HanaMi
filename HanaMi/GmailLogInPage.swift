ad4532216b379520649cd3fbf074c6816e2b15e1
Elle-Lo <tzuningluo@gmail.com>
2024-09-14 13:33:44 +0800

    Build Starter,LogIn,Home,Nav

---
+++ HanaMi/GmailLogInPage.swift
@@ -0,0 +1,22 @@
+//
+//  GmailLogInPage.swift
+//  HanaMi
+//
+//  Created by Tzu ning Lo on 2024/9/13.
+//
+
+import SwiftUI
+
+struct GmailLogInPage: View {
+    var body: some View {
+    VStack {
+        Text("Gmail Login Page")
+            .font(.largeTitle)
+            .padding()
+       
+        }
+    }
+}
+#Preview {
+    GmailLogInPage()
+}
